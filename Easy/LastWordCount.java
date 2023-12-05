ee355891a9592f6081c982a7999d3fac115ff703
Ralweena Lobo <95599990+Ralweena@users.noreply.github.com>
2023-12-05 14:41:16 +0530

    Updated LastWordCount.java

--- Easy/LastWordCount.java
+++ Easy/LastWordCount.java
@@ -1,10 +1,14 @@
+/* Given a string s consisting of words and spaces, Java Program to return the length of the last word in the string.*/
+
 import java.util.Scanner;
+
 class LastWordCount {
     public static int lastWordCount(String str) {
-        String[] strArr = str.split(" ");
+        String[] strArr = str.split(" ");  // Split the string to obtain the last word
         int n = strArr.length;
-        return strArr[n-1].length();
+        return strArr[n-1].length(); // returns the length of the last word
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
